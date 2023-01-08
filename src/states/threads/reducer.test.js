430f10f1e972b68632917a4212a735662b4adcfe
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-08 11:26:17 +0700

    feat: test reducer threads

---
+++ src/states/threads/reducer.test.js
@@ -0,0 +1,212 @@
+/* eslint-disable no-constant-condition */
+/**
+ * test scenario for threadReducer
+ *
+ * - threadReducer function
+ *  - should return the initial state when given by unknown action
+ *  - should return the threads when given by RECEIVE_THREADS action
+ *  - should return the threads with the new threads when given by ADD_THREAD action
+ *  - should return the threads with the toggled vote threads when given by UP_VOTE_THREAD action
+ *  - should return the threads with the toggled vote threads when given by DOWN_VOTE_THREAD action
+ *  - should return the threads with the toggled vote threads
+ *    when given by NEUTRAL_VOTE_THREAD action
+ *
+ */
+
+import threadsReducer from './reducer';
+
+describe('threadReducer function', () => {
+  it('should return the initial state', () => {
+    // arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(initialState);
+  });
+
+  it('should return threads when givent RECEIVE_THREADS action', () => {
+    // arrange
+    const initialState = [];
+    const action = {
+      type: 'RECEIVE_THREADS',
+      payload: {
+        threads: [
+          {
+            id: 'thread-1',
+            title: 'Thread Pertama',
+            body: 'Ini adalah thread pertama',
+            category: 'General',
+            createdAt: '2021-06-21T07:00:00.000Z',
+            ownerId: 'users-1',
+            upVotesBy: [],
+            downVotesBy: [],
+            totalComments: 0,
+          },
+          {
+            id: 'thread-2',
+            title: 'Thread Kedua',
+            body: 'Ini adalah thread kedua',
+            category: 'General',
+            createdAt: '2021-06-21T07:00:00.000Z',
+            ownerId: 'users-2',
+            upVotesBy: [],
+            downVotesBy: [],
+            totalComments: 0,
+          },
+        ],
+      },
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(action.payload.threads);
+  });
+
+  it('should return the threads with the new threads when given by ADD_THREAD action', () => {
+    const initialState = [
+      {
+        id: 'thread-1',
+        title: 'Thread Pertama',
+        body: 'Ini adalah thread pertama',
+        category: 'General',
+        createdAt: '2021-06-21T07:00:00.000Z',
+        ownerId: 'users-1',
+        upVotesBy: [],
+        downVotesBy: [],
+        totalComments: 0,
+      },
+    ];
+
+    const action = {
+      type: 'ADD_THREAD',
+      payload: {
+        thread: {
+          id: 'thread-2',
+          title: 'Thread Kedua',
+          body: 'Ini adalah thread kedua',
+          category: 'General',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          ownerId: 'users-2',
+          upVotesBy: [],
+          downVotesBy: [],
+          totalComments: 0,
+        },
+      },
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([action.payload.thread, ...initialState]);
+  });
+
+  it('should return the threads with the toggled vote threads when given by UP_VOTE_THREAD action', () => {
+    const initialState = [
+      {
+        id: 'thread-1',
+        title: 'Thread Pertama',
+        body: 'Ini adalah thread pertama',
+        category: 'General',
+        createdAt: '2021-06-21T07:00:00.000Z',
+        ownerId: 'users-1',
+        upVotesBy: [],
+        downVotesBy: [],
+        totalComments: 0,
+      },
+    ];
+
+    const action = {
+      type: 'UP_VOTE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'user-123',
+      },
+    };
+
+    // action: like talk
+    const nextState = threadsReducer(initialState, action);
+
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        upVotesBy: [action.payload.userId],
+      },
+    ]);
+  });
+
+  it('should return the threads with the toggled vote threads when given by DOWN_VOTE_THREAD action', () => {
+    const initialState = [
+      {
+        id: 'thread-1',
+        title: 'Thread Pertama',
+        body: 'Ini adalah thread pertama',
+        category: 'General',
+        createdAt: '2021-06-21T07:00:00.000Z',
+        ownerId: 'users-1',
+        upVotesBy: [],
+        downVotesBy: [],
+        totalComments: 0,
+      },
+    ];
+
+    const action = {
+      type: 'DOWN_VOTE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'user-123',
+      },
+    };
+
+    // action: like talk
+    const nextState = threadsReducer(initialState, action);
+
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        downVotesBy: [action.payload.userId],
+      },
+    ]);
+  });
+
+  it('should return the threads with the toggled vote threads when given by NEUTRAL_VOTE_THREAD action', () => {
+    const initialState = [
+      {
+        id: 'thread-1',
+        title: 'Thread Pertama',
+        body: 'Ini adalah thread pertama',
+        category: 'General',
+        createdAt: '2021-06-21T07:00:00.000Z',
+        ownerId: 'users-1',
+        upVotesBy: ['user-123'],
+        downVotesBy: [],
+        totalComments: 0,
+      },
+    ];
+
+    const action = {
+      type: 'NEUTRAL_VOTE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'user-123',
+      },
+    };
+
+    // action: like talk
+    const nextState = threadsReducer(initialState, action);
+    console.log(nextState);
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        upVotesBy: [],
+        downVotesBy: [],
+      },
+    ]);
+  });
+});
