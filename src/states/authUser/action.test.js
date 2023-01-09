01eb3797454f1ab25b946eec36345b5f0f732fee
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-09 22:47:00 +0700

    make test error

--- src/states/authUser/action.test.js
+++ src/states/authUser/action.test.js
@@ -50,7 +50,8 @@ describe('asyncReceiveLeadeBoard thunk', () => {
     await asyncSetAuthUser({ email: 'mahmud@gmail.com', password: 'secret' })(dispatch);
     // assert
     expect(dispatch).toHaveBeenCalledWith(showLoading());
-    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(fakeAuthUserResponse));
+    // expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(fakeAuthUserResponse));
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator('tes'));
     expect(dispatch).toHaveBeenCalledWith(hideLoading());
   });
 
