7e6971682270dea2d57d29c06cf4725a38d0487a
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-09 22:26:26 +0700

    feat ci

---
+++ src/stories/Input.stories.js
@@ -0,0 +1,16 @@
+import Navigation from '../components/Navigation';
+
+const stories = {
+  title: 'Input',
+  component: Navigation,
+};
+
+export default stories;
+
+const Default = () => (
+  <Navigation
+    title="input"
+  />
+);
+
+export { Default };
