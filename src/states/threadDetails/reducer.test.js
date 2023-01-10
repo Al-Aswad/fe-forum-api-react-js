8b7f2b92c9fe4be6bbb58d856dd0cd7f7d46587e
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-10 20:37:25 +0700

    add test case error

---
+++ src/states/threadDetails/reducer.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-constant-condition */
+
+import threadDetailReducer from './reducer';
+
+/**
+ * test scenario for threadDetailsReducer
+ *
+ *  - threadDetailsReducer function
+ *  - should return the initial state when given by unknown action
+ *  - should return the threads when given by RECEIVE_THREAD_DETAIL action
+ *
+ */
+describe('threadDetailsReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+
+    // action
+    const nextState = threadDetailReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual({});
+  });
+});
